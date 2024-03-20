7aa7d23de3bab13cfb29725dd90da156b520a9b5
zerty <zerty@users.noreply.github.com>
2024-03-20 17:49:00 +0100

    Create wuxiaone.py

---
+++ sources/en/w/wuxiaone.py
@@ -0,0 +1,7 @@
+import logging
+from lncrawl.templates.novelmtl import NovelMTLTemplate
+
+logger = logging.getLogger(__name__)
+
+class WuxiaOneCrawler(NovelMTLTemplate):
+    base_url = "https://www.wuxiaone.com"
